0dbeb891e19ddb4f93f0ea9fc3d35f4d263ba38a
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2021-05-12 18:19:20 -0400

    submit event created

---
+++ quiz.js
@@ -0,0 +1,21 @@
+const correctAnswers=['B', 'A', 'B', 'A']
+const form =document.querySelector('.quiz-form')
+const result=document.querySelector('.result')
+const displayScore=document.querySelector('.score')
+
+form.addEventListener('submit', e=>{
+
+    e.preventDefault()
+    let score=0;
+    const userAnswers=[form.q1.value, form.q2.value, form.q3.value, form.q4.value];
+
+    userAnswers.forEach((answer, index)=>{
+
+        if (answer===correctAnswers[index]){
+            score +=25;
+    
+        }
+    })
+    result.classList.remove('d-none')
+    displayScore.textContent=`${score}%`
+})
